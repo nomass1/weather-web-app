997f0af1e42981af98321cfe1d18505767155e66
nomass1 <watcharawit9669@gmail.com>
2024-07-14 05:45:28 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const wrapper = document.querySelector(".wrapper"),
   arrowBack = wrapper.querySelector("header i");
 
 let api;
-let apiKey = "b190a0605344cc4f3af08d0dd473dd25";
+let apiKey = "a2f3175ee08c2d23a76de35d3a14ccb5";
 
 const weatherChartCtx = document.getElementById("weatherChart").getContext("2d");
 let weatherChart;
